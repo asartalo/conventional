a1836e86b359647342a20108475fb4066fce904e
Wayne Duran <asartalo@gmail.com>
2021-02-26 16:03:52 +0800

    docs: document buildlessVersion

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,5 +1,6 @@
 part of '../conventional.dart';
 
+/// Generates a version that doesn't have build numbers
 Version buildlessVersion(Version version) {
   return Version.parse(
     '$version'.replaceAll(RegExp(r'\+.+$'), ''),
